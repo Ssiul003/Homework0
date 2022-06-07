900f4ae40a3e065b974d291732c3b05bbb1d21dc
Ssiul003 <106830829+Ssiul003@users.noreply.github.com>
2022-06-07 12:08:45 -0400

    Create MyNameis.java

---
+++ MyNameis.java
@@ -0,0 +1,16 @@
+package mynameis;
+
+  import java.util.Scanner;
+  
+    public class MyNameis{
+      public static void main (String[]args){
+      String first,last;
+      Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter your first and last name.");
+        System.out.println("Don't forget to press the enter key.");
+        
+        first= sc.next();
+        last=sc.next();
+        
+        System.out.println("My name is :"+ first +" "+last):
+  
